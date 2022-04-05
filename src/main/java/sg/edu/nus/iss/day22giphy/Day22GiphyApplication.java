fccfc5c3bc2884d40b255f7a5c2822748e33190c
tngye <tngyeefong@gmail.com>
2022-04-05 11:05:35 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/day22giphy/Day22GiphyApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day22giphy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day22GiphyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day22GiphyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/day22giphy/Day22GiphyApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day22giphy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day22GiphyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
